f052137e5c0ecd5ac3f40d649ec1e5a199be75c9
Bogdan Chaikovsky <bogdan.chaikovsky@belprog.com>
2017-08-18 17:18:55 +0300

    Added keyboard controller.

---
+++ Currency Converter/Keyboard.swift
@@ -0,0 +1,68 @@
+//
+//  Keyboard.swift
+//  Currency Converter
+//
+//  Created by Богдан Чайковский on 18.08.17.
+//  Copyright © 2017 Богдан Чайковский. All rights reserved.
+//
+
+import UIKit
+
+class Keyboard: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func pressedOne(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedTwo(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedThree(_ sender: UIButton) {
+        
+    }
+
+    @IBAction func pressedFour(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedFive(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func pressedSix(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedSeven(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedEight(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedNine(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedDot(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedZero(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func pressedDelete(_ sender: UIButton) {
+        
+    }
+    
+}
